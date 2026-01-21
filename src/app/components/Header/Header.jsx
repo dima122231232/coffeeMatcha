4fefe571168038469fd885808a71f645286da90a
Dima <dimokmilok10@gmail.com>
2026-01-21 23:49:28 +0100

    fix lag burger v0.4

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -10,40 +10,18 @@ gsap.registerPlugin(CustomEase);
 const ease = CustomEase.create("menu", "M0,0 C.7,0 .3,1 1,1");
 
 export default function Header() {
-  const helper = useRef(null);
-  const burger = useRef(null);
-  const lineA = useRef(null);
-  const lineB = useRef(null);
-  const open = useRef(false);
+    const helper = useRef(null), burger = useRef(null), lineA = useRef(null), lineB = useRef(null), open = useRef(false);
 
-  const toggle = () => {
-    const isOpen = open.current;
+    const toggle = () => {
+        const o = open.current;
 
-    // Анимация полосок бургера
-    gsap.to([lineA.current, lineB.current], {
-      duration: 0.25,
-      rotate: (index) => (isOpen ? 0 : index ? 45 : -45),
-      // смещение полосок вверх и вниз
-      transform: (index) =>
-        isOpen ? "translateY(0%)" : index ? "translateY(-50%)" : "translateY(50%)",
-    });
+        gsap.to([lineA.current, lineB.current], {duration: 0.25,rotate: (i) => (o ? 0 : (i ? 45 : -45)),transform: (i) => (o ? "translateY(0%)" : i ? "translateY(-50%)" : "translateY(50%)"),});
+        gsap.to(burger.current, { duration: 0.25, gap: o ? 5 : 0, ease });
+        helper.current.classList.toggle("open");
+        open.current = !o;
+    };
 
-    // Изменение расстояния между полосками бургера
-    gsap.to(burger.current, {
-      duration: 0.25,
-      gap: isOpen ? 5 : 0,
-      ease,
-    });
-
-    // переключаем класс open у помощника: CSS‑переходы займутся размерами
-    helper.current.classList.toggle("open");
-
-    open.current = !isOpen;
-  };
-
-  useEffect(() => {
-    splitTextToSpans(".StartAnimation");
-  }, []);
+    useEffect(() => {splitTextToSpans(".StartAnimation");}, []);
 
   return (
     <header className="header header--desktop">
@@ -52,20 +30,12 @@ export default function Header() {
         <div className="header__actions">
           <button className="header__button StartAnimation">buy now</button>
 
-          <button
-            className="header__burger header__burger--desktop"
-            ref={burger}
-            onClick={toggle}
-          >
+          <button className="header__burger header__burger--desktop" ref={burger} onClick={toggle}>
             <div className="header__burger__polosa" ref={lineA} />
             <div className="header__burger__polosa" ref={lineB} />
           </button>
 
-          {/* элемент, размеры которого изменяются */}
-          <div
-            className="header__burger--desktop-helper"
-            ref={helper}
-          />
+          <div className="header__burger--desktop-helper" ref={helper} />
         </div>
       </div>
     </header>

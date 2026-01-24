cdeabfb958a4da849afc419291f912fc234a8b60
Dima <dimokmilok10@gmail.com>
2026-01-25 00:25:25 +0100

    mobile fix v.8

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,3 @@
-"use client";
-
 import "@/styles/fonts.css";
 import "@/styles/globals.css";
 import "@/styles/adaptiv.css";
@@ -7,16 +5,19 @@ import SmoothScrolling from "@/app/SmoothScrolling";
 import Header from "@/app/components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+export const metadata = {
+    title: "Coffee Matcha",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <title>Coffee Matcha</title>
       <body >
         <Header/>
         <SmoothScrolling>
           {children}
         </SmoothScrolling>
         <Footer/>
+        <div className="footer-spacer" aria-hidden="true" />
       </body>
     </html>
   );
